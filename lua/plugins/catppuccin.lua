fc04bdac3bb7a0d03e8231940ccf4df9601a2662
Kyle Nel <22053773+kdevnel@users.noreply.github.com>
2024-02-05 17:05:17 +0200

    Update theme settings and fix some formatting

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,15 +1,22 @@
 -- catppuccin theme
 return {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        -- setup catppuccin theme
-        -- alternative schemes:
-        -- catppuccin-latte
-        -- catppuccin-frappe
-        -- see repo for more
-        vim.cmd.colorscheme "catppuccin"
-    end
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			integrations = {
+				alpha = true,
+				mason = true,
+				neotree = true,
+			},
+		})
+		-- setup catppuccin theme
+		-- alternative schemes:
+		-- catppuccin-latte
+		-- catppuccin-frappe
+		-- see repo for more
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
