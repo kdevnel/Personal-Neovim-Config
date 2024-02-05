1b011d565af5427f2b50e2880c818306a6fc151e
Kyle Nel <22053773+kdevnel@users.noreply.github.com>
2024-02-05 13:46:37 +0200

    Add none-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,17 +4,18 @@ return {
     config = function()
         local config = require('nvim-treesitter.configs')
         config.setup({
-            ensure_installed = {
-                "lua",
-                "javascript",
-                "php",
-                "css",
-                "html",
-                "typescript",
-                "markdown",
-                "markdown_inline",
-                "scss"
-            },
+            -- ensure_installed = {
+                --"lua",
+                --"javascript",
+                --"php",
+                --"css",
+                --"html",
+                --"typescript",
+                --"markdown",
+                --"markdown_inline",
+                --"scss"
+            --},
+            auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
         })
